54930e60768305e4fd7c2a967263da634c001b69
Irdgyu <gilangbayuirwana@gmail.com>
2025-04-30 13:07:51 +0700

    Pertanyaan nomor 1

--- StackTugasMahasiswa06.java
+++ StackTugasMahasiswa06.java
@@ -55,7 +55,7 @@ public class StackTugasMahasiswa06 {
     }
 
     public void print() {
-        for (int i = top; i <= 0; i--) {
+        for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
         System.out.println("");
